e24de5ad3149797a5507921d911e55cb85a03455
KirillAsmanov <mazusik1@gmail.com>
2020-02-28 16:48:26 +0300

    4.2. Тернарное сравнение[#244894] Произведено тернарное сравнение; All tests passed

---
+++ src/ru/job4j/Test/MaxTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.Test;
+
+import org.junit.Test;
+import ru.job4j.condition.Max;
+import org.junit.Assert;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MaxTest {
+    @Test
+    public void whenMax1To2Then2() {
+        assertThat(Max.max(1, 2), is(2));
+    }
+
+    @Test
+    public void whenMax4To3Then4() {
+        assertThat(Max.max(4,3), is(4));
+    }
+
+    @Test
+    public void when6AreEquals6Then6 (){
+        assertThat(Max.max(6,6), is(6));
+    }
+}
