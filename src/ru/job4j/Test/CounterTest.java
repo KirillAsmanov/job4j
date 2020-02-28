002d56b99eaa9a6af0016c1231dd87fbf68e430d
KirillAsmanov <mazusik1@gmail.com>
2020-02-29 00:36:28 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне[#244975] В класс Counter добавлен метод sumByEven, находящий сумму четных чисел в диапазоне. All tests passed.

---
+++ src/ru/job4j/Test/CounterTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.Test;
+
+import org.junit.Test;
+import ru.job4j.loop.Counter;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int rsl = Counter.sumByEven(1, 10);
+        int expected = 30;
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenSumEvenNumFrom10To12Is22(){
+        assertThat(Counter.sumByEven(10,12), is(22));
+    }
+}
