3fad59a51e97238e8bef80c4e7f97a46ec3b4499
KirillAsmanov <mazusik1@gmail.com>
2020-03-02 02:02:59 +0300

    6.2.1. Выход за границу массива.[#244920]

---
+++ src/ru/job4j/array/SwitchArray.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+/**
+ * упр. 6.2.1. Выход за границу массива.[#244920]
+ * @author Kirill Asmanov
+ * @since 01.03.2020
+ * @version 1.0.0;
+ */
+
+public class SwitchArray {
+    /**
+     * Ошибка была в том, что максимальный индекс массива равен не его длине, а (длине - 1).
+     * @param array - входящий массив чисел
+     * @return массив, в котором поменяны местами нулевой и последний элемент
+     */
+    public static int[] swapBorder(int[] array) {
+        int temp = array[0];
+        array[0] = array[array.length-1];
+        array[array.length-1] = temp;
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {1, 2, 3, 4, 5, 6};
+        int[] rsl = swapBorder(nums);
+        for (int index = 0; index < rsl.length; index++) {
+            System.out.println(rsl[index]);
+        }
+    }
+}
