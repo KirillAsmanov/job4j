7550eccb0b0a1df4a71af4cb13b791a44708ef9f
KirillAsmanov <mazusik1@gmail.com>
2020-03-02 01:26:25 +0300

    5.7. Ипотека.[#244970] Входные параметры теперь имеют типы, соответствующие заданию

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -1,10 +1,11 @@
 package ru.job4j.loop;
 
 public class Mortgage {
-    public int year(double amount, int salary, double percent) {
+    public int year(int amount, int salary, double percent) {
         int year = 0;
-        while (amount>0){
-            amount = amount + amount*(percent/100) - salary;
+        double currentSum = amount;
+        while (currentSum>0){
+            currentSum = currentSum + currentSum*(percent/100) - salary;
             year+=1;
         }
         return year;
