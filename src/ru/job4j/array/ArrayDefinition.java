e7e05635567f41f64debe4834cec1e061e3f0290
KirillAsmanov <mazusik1@gmail.com>
2020-03-01 20:35:51 +0300

    6.0.1. Объявление массива.[#244924]

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+/**
+ * Класс для создания массивов, упр. 6.0.1. Объявление массива.[#244924]
+ * @author Kirill Asmanov
+ * @since 01.02.2020
+ * @version 1.0.0;
+ */
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
---
+++ src/ru/job4j/array/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for array tasks.
+ * 01.03.2020
+ * @author Asmanov Kirill (mazusik1@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.array;
