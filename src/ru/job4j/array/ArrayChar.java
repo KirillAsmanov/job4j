151e792cd6fae8de36eff508f1827172265e96fe
KirillAsmanov <mazusik1@gmail.com>
2020-03-02 04:19:44 +0300

    6.4. Слово начинается с ...[#244904]

---
+++ src/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+/**
+ * 6.4. Слово начинается с ...[#244904]
+ * @author Kirill Asmanov
+ * @since 02.03.2020
+ * @version 1.0.0;
+ */
+public class ArrayChar {
+    /**
+     * Этот метод проверяет, что слово начинается c определенной последовательности
+     * @param word - проверяемое слово
+     * @param pref - последовательность символов, с которого начинается или не начинается слово word
+     * @return true, если начинается, false - если нет
+     */
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = false;
+
+        for (int i=0;i<pref.length;i++){
+            result = (word[i] == pref[i]);
+            if (!result) {
+                break;
+            }
+        }
+
+        return result;
+    }
+}
