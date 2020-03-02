6afae609055a5ea7a9d320f4380ce302356c7632
KirillAsmanov <mazusik1@gmail.com>
2020-03-02 05:03:34 +0300

    6.4.1. Слово заканчивается с ...[#244908]

---
+++ src/ru/job4j/array/EndsWith.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+/**
+ * 6.4.1. Слово заканчивается с ...[#244908]
+ * @author Kirill Asmanov
+ * @since 02.03.2020
+ * @version 1.0.0;
+ */
+public class EndsWith {
+    /**
+     * Этот метод проверяет, что слово заканчивается на определенную последовательность
+     * @param word - проверяемое слово
+     * @param pref - последовательность символов, на которую  заканчивается или не заканчивается слово word
+     * @return true, если заканчивается, false - если нет
+     */
+    public static boolean endsWith(char[] word, char[] pref) {
+        boolean result = false;
+
+        for (int i=0;i<pref.length;i++){
+            result = (word[word.length - pref.length + i] == pref[i]);
+            if (!result) {
+                break;
+            }
+        }
+
+        return result;
+    }
+}
